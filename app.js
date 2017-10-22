c216d3ca970fcb7136a57c091c9fe774ec7654e7
jimenezbr7 <jimenezbr7@gmail.com>
2017-10-22 15:13:05 -0400

    adding the first assignment

---
+++ app.js
@@ -0,0 +1,12 @@
+// this is solving part 1 of the assignment
+var number1 = 5
+var number2 = 22
+
+console.log(number1 + number2);
+
+var string1 = "hey"
+var string2 = "how's it going"
+
+console.log(string1 + " " + string2);
+
+// this is solving part 2 of the assignment
